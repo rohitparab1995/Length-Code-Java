ebebe499280e60fe79cbdb1c3473392caaac5c30
rohitparab1995 <33113609+rohitparab1995@users.noreply.github.com>
2020-01-12 10:24:18 -0500

    Add files via upload

---
+++ CheckRecordSize.java
@@ -0,0 +1,36 @@
+package com.rohit;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class CheckRecordSize {                 
+    
+	public static void main(String[] args) throws IOException {
+	
+        FileInputStream fstream = new FileInputStream("C:\\Users\\n0281359\\Desktop\\Error Feeds\\ofac_integrity_py.dat");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+        String strLine;
+        int i=1;
+
+        ArrayList<Integer> not=new ArrayList<Integer>();
+		
+        while ((strLine = br.readLine()) != null)   
+		{
+        	if(strLine.getBytes().length != 198 )
+			{
+     		not.add(i);
+			}
+		i++;                                                                                                                                                                
+        }
+        
+		System.out.println(not);
+        System.out.println(not.size());
+        br.close();
+                                                
+        }
+
+    }
+
